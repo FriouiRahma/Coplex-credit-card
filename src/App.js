d740c491e174aa79f76eb66bee844b5a4f264b52
FriouiRahma <friouirahma9@gmail.com>
2020-02-18 13:37:20 +0100

    card commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Credit extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+      code:"",
+      name:"",
+      date:""
+
+    }
+      
+      }
+      handelChange=(e)=>{
+        this.setState({[e.target.name]:e.target.value})
+    
+        }
+
+  render() {
+    var regex=/^[0-9\b]+$/
+  
+    return (
+      <div className="gors">
+        <div className="nav"><h2>Compnay name</h2>
+        <h2>{regex.test(this.state.code) ?this.state.code.padEnd(16,"*").replace(/(\d{4})/g, '$1 ').replace(/(^\s+|\s+$)/,''):"******************"}</h2>
+        <h2>{this.state.name.toUpperCase()}</h2>
+        <h2>{this.state.date.padEnd(4,"*").replace(/(\d{2})(\d{0})/,'$1/$2')}</h2>
+        </div>
+        <div>
+        <input type="text"  name="code" onChange={this.handelChange} />
+        <input type="text"  name="name" onChange={this.handelChange} />
+        <input type="text"  name="date"  onChange={this.handelChange}  />
+
+        </div>
+
+
+      </div>
+
+    );
+
+  }
+
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Credit />
+
     </div>
   );
 }
---
+++ src/cardcomplex.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Creditcard=()=>{
+return (
+<div>
+<div className="nav"><h2>Compnay name</h2></div>
+<div className="nav"><h2>Logo Card</h2></div>
+
+
+
+</div>
+)
+
+
+}
+
+ 
+
+export default Creditcard;
+
+
+
